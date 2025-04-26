917b5b567ed93621df38f75db5a99979dde338c8
nagacharankumarreddy <nagacharankumarreddy@gmail.com>
2025-04-26 16:31:56 +0530

    HashRouter for gh page refresh issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // <<< changed
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/blood-mate">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
