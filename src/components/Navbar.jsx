2bbd565b51d11c649117a821dfd075a41e02bb18
nagacharankumarreddy <nagacharankumarreddy@gmail.com>
2025-04-26 20:02:28 +0530

    CSS fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <nav className="navbar">
-      <h2>BloodMate</h2>
-      <div className="navbar-links">
-        <Link to="/">Home</Link>
-        <Link to="/donors">Donors</Link>
-        <Link to="/register">Register As Donor</Link>
+      <div className="navbar-header">
+        <h2>BloodMate</h2>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+      </div>
+
+      <div className={`navbar-links ${menuOpen ? "open" : ""}`}>
+        <Link
+          to="/"
+          className={
+            location.pathname === "/" ? "nav-link active-link" : "nav-link"
+          }
+        >
+          Home
+        </Link>
+        <Link
+          to="/donors"
+          className={
+            location.pathname === "/donors"
+              ? "nav-link active-link"
+              : "nav-link"
+          }
+        >
+          Donors
+        </Link>
+        <Link
+          to="/register"
+          className={
+            location.pathname === "/register"
+              ? "nav-link active-link"
+              : "nav-link"
+          }
+        >
+          Register As Donor
+        </Link>
       </div>
     </nav>
   );
